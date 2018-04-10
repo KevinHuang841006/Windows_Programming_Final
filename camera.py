a8329973f278ceb88b23cb442212d8c186ffeac0
Kevin Huang <34711059+KevinHuang841006@users.noreply.github.com>
2018-04-10 23:06:32 +0800

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,140 @@
+from PyQt5 import QtCore
+from PyQt5 import QtWidgets
+from PyQt5 import QtGui
+
+import sys
+from os import path
+import time
+import cv2
+import numpy as np
+
+class RecordVideo(QtCore.QObject):
+    image_data = QtCore.pyqtSignal(np.ndarray)
+
+    def __init__(self, camera_port=0, parent=None):
+        super().__init__(parent)
+        self.camera = cv2.VideoCapture(camera_port)
+
+        self.timer = QtCore.QBasicTimer()
+
+    def start_recording(self):
+        self.timer.start(0, self)
+
+    def timerEvent(self, event):
+        if (event.timerId() != self.timer.timerId()):
+            return
+
+        read, data = self.camera.read()
+        if read:
+            self.image_data.emit(data)
+
+
+class FaceDetectionWidget(QtWidgets.QWidget):
+    def __init__(self, haar_cascade_filepath, parent=None):
+        super().__init__(parent)
+        self.classifier = cv2.CascadeClassifier(haar_cascade_filepath)
+        self.image = QtGui.QImage()
+        self._red = (0, 0, 255)
+        self._width = 2
+        self._min_size = (30, 30)
+
+    def detect_faces(self, image: np.ndarray):
+        # haarclassifiers work better in black and white
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray_image = cv2.equalizeHist(gray_image)
+
+        faces = self.classifier.detectMultiScale(gray_image,
+                                                 scaleFactor=1.3,
+                                                 minNeighbors=4,
+                                                 flags=cv2.CASCADE_SCALE_IMAGE,
+                                                 minSize=self._min_size)
+
+        return faces
+
+    def image_data_slot(self, image_data):
+        faces = self.detect_faces(image_data)
+        tests = np.array(image_data)
+        #print(tests.shape)
+        """
+          Edit here~~~~  image dim : 480 * 640 *3
+          teeth detection block~
+          image_data = reshape(image_data)         # to 224 * 224 * 3
+          id = teeth_detection(image_data)         # do teeth detection
+          print(id)                                # the id of teeth
+        """
+        tests = tests[:,80:560,:]
+        #cv2.imwrite('test.jpg',tests)
+		
+        #print(tests.shape)
+        time.sleep(0.03)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(image_data,
+                          (x, y),
+                          (x+w, y+h),
+                          self._red,
+                          self._width)
+
+        self.image = self.get_qimage(image_data)
+        if self.image.size() != self.size():
+            self.setFixedSize(self.image.size())
+
+        self.update()
+
+    def get_qimage(self, image: np.ndarray):
+        height, width, colors = image.shape
+        bytesPerLine = 3 * width
+        QImage = QtGui.QImage
+        #print(image.shape)
+        
+        image = QImage(image.data,
+                       width,
+                       height,
+                       bytesPerLine,
+                       QImage.Format_RGB888)
+
+        image = image.rgbSwapped()
+        return image
+
+    def paintEvent(self, event):
+        painter = QtGui.QPainter(self)
+        painter.drawImage(0, 0, self.image)
+        self.image = QtGui.QImage()
+
+
+class MainWidget(QtWidgets.QWidget):
+    def __init__(self, haarcascade_filepath, parent=None):
+        super().__init__(parent)
+        fp = haarcascade_filepath
+        self.face_detection_widget = FaceDetectionWidget(fp)
+
+        
+        self.record_video = RecordVideo()
+
+        image_data_slot = self.face_detection_widget.image_data_slot
+        self.record_video.image_data.connect(image_data_slot)
+
+        layout = QtWidgets.QVBoxLayout()
+
+        layout.addWidget(self.face_detection_widget)
+        self.run_button = QtWidgets.QPushButton('Start')
+        layout.addWidget(self.run_button)
+
+        self.run_button.clicked.connect(self.record_video.start_recording)
+        self.setLayout(layout)
+
+
+def main(haar_cascade_filepath):
+    app = QtWidgets.QApplication(sys.argv)
+
+    main_window = QtWidgets.QMainWindow()
+    main_widget = MainWidget(haar_cascade_filepath)
+    main_window.setCentralWidget(main_widget)
+    main_window.show()
+    sys.exit(app.exec_())
+
+#Start from heere~~~~
+if __name__ == '__main__':
+    
+    cascade_filepath = path.abspath('haarcascade_frontalface_default.xml')
+    #print(path.abspath(find()))
+    main(cascade_filepath)
